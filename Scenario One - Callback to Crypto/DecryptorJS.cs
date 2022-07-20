6e58a5f4cef1b23ed9081b976597fa8796a81d63
György Demeter <demeter.gyorgy@int.dss.hu>
2022-07-20 18:27:44 +0200

    feat: Scenario One - Callback to Crypto

---
+++ Scenario One - Callback to Crypto/DecryptorJS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Jint;
+
+namespace EnDecryptWithJint
+{
+    public static class DecryptorJS
+    {
+        /* 
+         * take care of multiple statements
+         * dont use if(a > b){x+y}; if(a < b){y-x}; -> before executing each statement JInt resets complition value
+         * use function
+         * use in every step "engine.Execute(...here your script...)"
+        */
+        private static string jsSource { get; set; }
+            = $@"function decryptString(encryptedText)
+                 {{
+                     var gKey = CryptoJS.enc.Utf8.parse(key);
+		             var gIV = CryptoJS.enc.Utf8.parse(iv);
+                     var decryptedbytes = CryptoJS.AES.decrypt(encryptedText, gKey, {{iv: gIV}});
+                     var decryptedtext = decryptedbytes.toString(CryptoJS.enc.Utf8);
+                     return ""Greetings "" + decryptedtext + ""!"";
+                 }};
+                                  
+                 decryptString(encryptedText);";
+
+        public static string DecryptString(string encryptedText, string key, string iv)
+        {
+            if (string.IsNullOrEmpty(encryptedText))
+                throw new ArgumentNullException("encryptedText");
+
+            var engine = new Engine();
+
+            var cryptojs = File.ReadAllText(@"crypto-v3.1.2.js");
+            engine.Execute(cryptojs);
+
+            engine.SetValue("encryptedText", encryptedText);
+            engine.SetValue("key",  key);
+            engine.SetValue("iv", iv);
+            var decryptedText = engine.Execute(jsSource).GetCompletionValue();
+
+            return decryptedText.ToString();
+        }
+    }
+}
---
+++ Scenario One - Callback to Crypto/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EnDecryptWithJint
+{
+    internal class Program
+    {
+        internal static string inputText = "CGMforever";
+
+        static void Main(string[] args)
+        {
+            var encryptedText = Encryptor.EncryptString(inputText);
+
+            if(string.IsNullOrEmpty(encryptedText))
+                throw new ArgumentNullException("encryptedText");
+
+            Console.WriteLine($"Origin text: {inputText}");
+
+            Console.WriteLine($"Encrypted text: {encryptedText}");
+
+            var decryptedText = DecryptorJS.DecryptString(encryptedText, Encryptor.Key, Encryptor.IV);
+
+            if (string.IsNullOrEmpty(decryptedText))
+                throw new ArgumentNullException("decryptedText");
+
+            // For test
+            //var decryptedText = Encryptor.DecryptString(encryptedText);
+            Console.WriteLine($"DecryptedText text: {decryptedText}");
+
+            Console.ReadKey();
+        }
+    }
+}
